2483c32ce95193c418723815272769eeb110aa3d
Jim Moua <jimmoua24@gmail.com>
2020-04-13 03:30:01 -0500

    Changed to concurrent hashmap. Added some getters/setters.

--- multiton/Store/Store.java
+++ multiton/Store/Store.java
@@ -1,36 +1,40 @@
 package multiton.Store;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 import multiton.Main;
 
 public class Store extends Thread {
 
   // Static mapping
-  private static HashMap<Location, Store> StoreMap = new HashMap<Location, Store>();
+  private static ConcurrentHashMap<Location, Store> StoreMap = new ConcurrentHashMap<Location, Store>();
 
   // Store information
   private int items = 5000;
+  private final static int MAX_STOCK = 20000;
+  private final static int RESTOCK_COUNT = 5000;
   private Location key; // Location of the store
   private int sales = 0;
+  public static volatile int ttlSales = 0;
 
-  private synchronized void restockShelf() {
-    items+=5000;
-    if (items > 20000)
-      items = 20000;
+  private void restockShelf() {
+    StoreMap.get(key).items+=RESTOCK_COUNT;
+    if(StoreMap.get(key).items > MAX_STOCK) StoreMap.get(key).items = MAX_STOCK;
+    updateStock();
   }
 
-  final public int getItemStock() {
-    return items;
+  public int getItemStock() {
+    return StoreMap.get(key).items;
   }
 
-  public synchronized void sellItems(final int itemCount) {
-    items-=itemCount;
-    sales++;
+  public void sellItems(final int itemCount) {
+    StoreMap.get(key).items-=itemCount;
+    updateStock();
+    StoreMap.get(key).sales++;
   }
 
   public static synchronized Store getStore(final Location key) {
-    if (StoreMap.get(key) == null) {
+    if(StoreMap.get(key) == null) {
       StoreMap.put(key, new Store(key));
     }
     return StoreMap.get(key);
@@ -42,19 +46,29 @@ public class Store extends Thread {
     start();
   }
 
-  public final int getSales() {
-    return sales;
+  public int getSales() {
+    return StoreMap.get(key).sales;
+  }
+
+  private synchronized void updateStock() {
+    GUI.itemStockGUI.forEach((k, v) -> {
+      if(k == key) {
+        v.itemStoreStockTextField.setText(String.format("%d items in stock", items));
+      }
+    });
   }
 
   public void run() {
     long restockTimer = System.currentTimeMillis();
-    while(!Main.customerList.isEmpty()) {
+    while(!Main.getCustomerList().isEmpty()) {
       final long nowTimer = System.currentTimeMillis();
-      if(nowTimer-restockTimer >= 3000L) {
+      // Restock every three seconds
+      if(nowTimer-restockTimer >= 3000) {
         restockShelf();
-        System.out.printf("Restocking store location at %s...\n", key.toString());
         restockTimer = System.currentTimeMillis();
       }
     }
+    ttlSales+=StoreMap.get(key).sales;
+    System.out.printf("Store %s made %d sales.\n", key.toString(), StoreMap.get(key).sales);
   }
 }
