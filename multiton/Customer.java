30e65f25627fb08d5cb62fb099f9eaaa86f7aafe
Jim Moua <jimmoua24@gmail.com>
2020-04-13 03:28:25 -0500

    Syncronized goShopping function. Made purchased to 10 and increased run sleep time.

--- multiton/Customer.java
+++ multiton/Customer.java
@@ -1,50 +1,47 @@
 package multiton;
 
 import java.lang.Math;
-import multiton.Store.*;
+
+import multiton.Store.GUI;
+import multiton.Store.Location;
+import multiton.Store.Store;
 
 public class Customer extends Thread {
-  private static int CustomerIDCount = 0;
-  private int customerID;
   private int purchased = 0;
 
   public Customer() {
-    customerID = ++CustomerIDCount;
-    start();
   }
 
-  private void goShopping() {
-    // Randomly choose item quantity to buy.
-    final int itemsToPurchase = (int) (Math.random() * 101 + 50);
-    boolean transactionComplete = false;
-    while (!transactionComplete) {
-      for (Location i : Location.values()) {
-        if (Store.getStore(i).getItemStock() >= itemsToPurchase) {
-          System.out.printf("Customer %d purchased %d unites.\n", customerID, itemsToPurchase);
-          Store.getStore(i).sellItems(itemsToPurchase);
-          transactionComplete = true;
-          break;
-        }
+  private static synchronized void goShopping() {
+    for(Location i : Location.values()) {
+      // Randomly choose item quantity to buy.
+      final int itemsToPurchase = (int) (Math.random() * 101 + 50);
+      if(Store.getStore(i).getItemStock() >= itemsToPurchase) {
+        Store.getStore(i).sellItems(itemsToPurchase);
+        return;
+      } else if(Store.getStore(i).getItemStock() < 0) {
+        System.out.printf("Error: negative number reached.\n");
+        System.exit(1);
       }
     }
   }
 
   public void run() {
-    while (purchased < 5) {
+    while(Main.getCustomerList().size() < Main.numCustomers);
+    while(purchased < 10) {
       final int numGen = (int) (Math.random() * 5 + 1);
-      if (numGen != 1) {
+      if(numGen != 1) {
         goShopping();
         purchased++;
       }
       try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
+        sleep(1500);
+      } catch(InterruptedException e) {
         e.printStackTrace();
       }
     }
-    Main.customerList.remove(this);
-    System.out.printf("Customer %d has left the store.\n", customerID);
-    System.out.printf("There are %d customers left.\n", Main.customerList.size());
+    GUI.getTField().setText(String.valueOf(Main.getCustomerList().size()-1));
+    Main.getCustomerList().remove(this);
   }
 
 }
