2d0740bf25bfc7d6ea4f7ed75b97ae2ec9e4504d
Jim Moua <jimmoua24@gmail.com>
2020-04-18 12:06:14 -0500

    Cleaned up comments.

--- multiton/Customer.java
+++ multiton/Customer.java
@@ -13,10 +13,12 @@ public class Customer {
 
   public void goShopping() {
     for(Location i : Location.values()) {
-      // Randomly choose item quantity to buy.
-      final int itemsToPurchase = (int) (Math.random() * 101 + 50); // [50, 150]
+      // Generate item quantity [50, 150] to buy.
+      final int itemsToPurchase = (int) (Math.random() * 101 + 50);
       if(Store.getStore(i).getItemStock() >= itemsToPurchase) {
+        // Generate number [1, 5]
         final int numGen = (int) (Math.random() * 5 + 1);
+        // If number isn't 1, purchase the amount specified above.
         if(numGen != 1) {
           Store.getStore(i).sellItems(itemsToPurchase);
           GUI.getTField().setText(String.valueOf(Main.customerList.size()-1));
